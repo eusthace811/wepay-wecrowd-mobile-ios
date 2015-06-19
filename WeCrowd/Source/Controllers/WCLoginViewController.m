557bcaa1ba01a6aacdfac29d5d25101ed928e6cb
Zach Vega-Perkins <zachv@wepay.com>
2015-06-19 13:23:16 -0700

    Added IBAction for merchant login.

--- WeCrowd/Source/Controllers/WCLoginViewController.m
+++ WeCrowd/Source/Controllers/WCLoginViewController.m
@@ -16,16 +16,21 @@
 
 #pragma mark - UIViewController
 
-- (void)viewDidLoad {
+- (void) viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void) didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - IBOutlets
+
+- (IBAction) login:(id)sender {
+
+}
+
 
 @end
