ede8d9358ad58ef9ab1dc93dcf9681e7a7d97a4e
Zach Vega-Perkins <zachv@wepay.com>
2015-06-25 10:32:31 -0700

    Initial commit of login manager

---
+++ WeCrowd/Source/Managers/WCLoginManager.h
@@ -0,0 +1,13 @@
+//
+//  WCLoginManager.h
+//  WeCrowd
+//
+//  Created by Zach Vega-Perkins on 6/25/15.
+//  Copyright (c) 2015 WePay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WCLoginManager : NSObject
+
+@end
---
+++ WeCrowd/Source/Managers/WCLoginManager.m
@@ -0,0 +1,13 @@
+//
+//  WCLoginManager.m
+//  WeCrowd
+//
+//  Created by Zach Vega-Perkins on 6/25/15.
+//  Copyright (c) 2015 WePay. All rights reserved.
+//
+
+#import "WCLoginManager.h"
+
+@implementation WCLoginManager
+
+@end
