0848d60e041f0287fe2b0654920e9b4801f37607
Zach Vega-Perkins <zachv@wepay.com>
2015-06-25 13:48:29 -0700

    Created campaign detail VC.

---
+++ WeCrowd/Source/Controllers/WCCampaignDetailViewController.m
@@ -0,0 +1,27 @@
+//
+//  WCCampaignDetailViewController.m
+//  WeCrowd
+//
+//  Created by Zach Vega-Perkins on 6/25/15.
+//  Copyright (c) 2015 WePay. All rights reserved.
+//
+
+#import "WCCampaignDetailViewController.h"
+
+@interface WCCampaignDetailViewController ()
+
+@end
+
+@implementation WCCampaignDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
