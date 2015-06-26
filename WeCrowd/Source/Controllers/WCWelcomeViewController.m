b328aa39533720c150e939bf3f6fc8d7c047bc45
Zach Vega-Perkins <zachv@wepay.com>
2015-06-26 14:34:00 -0700

    Created welcome VC

---
+++ WeCrowd/Source/Controllers/WCWelcomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  WCWelcomeViewController.m
+//  WeCrowd
+//
+//  Created by Zach Vega-Perkins on 6/26/15.
+//  Copyright (c) 2015 WePay. All rights reserved.
+//
+
+#import "WCWelcomeViewController.h"
+
+@interface WCWelcomeViewController ()
+
+@end
+
+@implementation WCWelcomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
