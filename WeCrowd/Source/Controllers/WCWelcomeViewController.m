9c9283e24c88bd9e0ac2e6dba3f0d2fd7e253fab
Zach Vega-Perkins <zachv@wepay.com>
2015-06-26 14:34:59 -0700

    Added segue prep method to welcome VC.

--- WeCrowd/Source/Controllers/WCWelcomeViewController.m
+++ WeCrowd/Source/Controllers/WCWelcomeViewController.m
@@ -24,7 +24,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -32,6 +31,5 @@
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
 
 @end
