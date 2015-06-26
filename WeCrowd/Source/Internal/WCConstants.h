15ec9d12b8bbd51cbeaaed29ca3ad596d8ea5d65
Zach Vega-Perkins <zachv@wepay.com>
2015-06-26 15:39:55 -0700

    Added constants file.

---
+++ WeCrowd/Source/Internal/WCConstants.h
@@ -0,0 +1,13 @@
+//
+//  WCConstants.h
+//  WeCrowd
+//
+//  Created by Zach Vega-Perkins on 6/26/15.
+//  Copyright (c) 2015 WePay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WCConstants : NSObject
+
+@end
---
+++ WeCrowd/Source/Internal/WCConstants.m
@@ -0,0 +1,13 @@
+//
+//  WCConstants.m
+//  WeCrowd
+//
+//  Created by Zach Vega-Perkins on 6/26/15.
+//  Copyright (c) 2015 WePay. All rights reserved.
+//
+
+#import "WCConstants.h"
+
+@implementation WCConstants
+
+@end
