ec6072108ff30e9d9948d65dcad16b8702f94ec8
Zach Vega-Perkins <zachv@wepay.com>
2015-06-30 14:54:09 -0700

    Connected touch event to ManualPayment VC.

--- WeCrowd/Source/Controllers/WCManualPaymentViewController.m
+++ WeCrowd/Source/Controllers/WCManualPaymentViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "WCManualPaymentViewController.h"
+#import "WCWePayManager.h"
 
 @interface WCManualPaymentViewController ()
 
@@ -14,7 +15,8 @@
 
 @implementation WCManualPaymentViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
@@ -24,14 +26,9 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)submitInformationAction:(id) sender
+{
+    
 }
-*/
 
 @end
