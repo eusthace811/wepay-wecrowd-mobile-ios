075e440162dbcbb2026859212f96a3bb265e5560
Zach Vega-Perkins <zachv@wepay.com>
2015-06-30 10:08:54 -0700

    Created ManualEntry VC.

---
+++ WeCrowd/Source/Controllers/WCManualPaymentViewController.h
@@ -0,0 +1,13 @@
+//
+//  WCManualPaymentViewController.h
+//  WeCrowd
+//
+//  Created by Zach Vega-Perkins on 6/30/15.
+//  Copyright (c) 2015 WePay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WCManualPaymentViewController : UIViewController
+
+@end
