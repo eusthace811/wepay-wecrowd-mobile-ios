d98ac29b878faec6ab36a15ddc4826fb56bd7cd3
Zach Vega-Perkins <zachv@wepay.com>
2015-06-30 15:48:34 -0700

    Created View class for credit card UI.

---
+++ WeCrowd/Source/Views/WCCreditCardInfoEntryView.h
@@ -0,0 +1,13 @@
+//
+//  WCCreditCardInfoEntryView.h
+//  WeCrowd
+//
+//  Created by Zach Vega-Perkins on 6/30/15.
+//  Copyright (c) 2015 WePay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WCCreditCardInfoEntryView : UIView
+
+@end
