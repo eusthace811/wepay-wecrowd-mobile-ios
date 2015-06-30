2b2d2a1aa6c13d4fbee98a826d135310283580fb
Zach Vega-Perkins <zachv@wepay.com>
2015-06-30 10:16:30 -0700

    Created WePay manager class.

---
+++ WeCrowd/Source/Managers/WCWePayManager.m
@@ -0,0 +1,13 @@
+//
+//  WCWePayManager.m
+//  WeCrowd
+//
+//  Created by Zach Vega-Perkins on 6/30/15.
+//  Copyright (c) 2015 WePay. All rights reserved.
+//
+
+#import "WCWePayManager.h"
+
+@implementation WCWePayManager
+
+@end
