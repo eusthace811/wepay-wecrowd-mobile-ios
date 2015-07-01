22c7403e79f91133e814cfdd72bee8ce797f7374
Zach Vega-Perkins <zachv@wepay.com>
2015-07-01 15:50:08 -0700

    Created donation manager.

---
+++ WeCrowd/Source/Managers/WCDonationManager.h
@@ -0,0 +1,13 @@
+//
+//  WCDonationManager.h
+//  WeCrowd
+//
+//  Created by Zach Vega-Perkins on 7/1/15.
+//  Copyright (c) 2015 WePay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WCDonationManager : NSObject
+
+@end
---
+++ WeCrowd/Source/Managers/WCDonationManager.m
@@ -0,0 +1,13 @@
+//
+//  WCDonationManager.m
+//  WeCrowd
+//
+//  Created by Zach Vega-Perkins on 7/1/15.
+//  Copyright (c) 2015 WePay. All rights reserved.
+//
+
+#import "WCDonationManager.h"
+
+@implementation WCDonationManager
+
+@end
