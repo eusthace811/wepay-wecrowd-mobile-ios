2ca56fd8fc5e40cbb9567bad871f0a612fd63019
Zach Vega-Perkins <zachv@wepay.com>
2015-07-08 10:47:50 -0700

    Added alert class.

---
+++ WeCrowd/Source/Internal/WCAlerts.h
@@ -0,0 +1,13 @@
+//
+//  WCAlerts.h
+//  WeCrowd
+//
+//  Created by Zach Vega-Perkins on 7/8/15.
+//  Copyright (c) 2015 WePay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WCAlerts : NSObject
+
+@end
---
+++ WeCrowd/Source/Internal/WCAlerts.m
@@ -0,0 +1,13 @@
+//
+//  WCAlerts.m
+//  WeCrowd
+//
+//  Created by Zach Vega-Perkins on 7/8/15.
+//  Copyright (c) 2015 WePay. All rights reserved.
+//
+
+#import "WCAlerts.h"
+
+@implementation WCAlerts
+
+@end
